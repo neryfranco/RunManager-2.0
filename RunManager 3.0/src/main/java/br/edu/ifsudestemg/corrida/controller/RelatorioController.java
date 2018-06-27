41d9b07aed34cc61eb90b2915c49456d86c4cbcb
VictorDSilva <victordomingos91@gmail.com>
2018-06-27 17:37:33 -0300

    Relatórios

--- RunManager 3.0/src/main/java/br/edu/ifsudestemg/corrida/controller/RelatorioController.java
+++ RunManager 3.0/src/main/java/br/edu/ifsudestemg/corrida/controller/RelatorioController.java
@@ -1,8 +1,10 @@
-/*
 package br.edu.ifsudestemg.corrida.controller;
 
+import br.edu.ifsudestemg.corrida.repository.CorridaRepository;
+import br.edu.ifsudestemg.corrida.repository.OrganizadorRepository;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.util.JRSaver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,22 +24,15 @@ import java.util.HashMap;
 @RequestMapping(value = "/relatorio")
 public class RelatorioController {
 
-    public JasperReport gerarJasper(String relatorio, String nome) throws JRException {
-        InputStream relatorioStream = getClass().getResourceAsStream(relatorio);
-        System.out.println("5******************************************");
-        if (relatorioStream == null)
-            return null;
-
-        JasperReport jasperReport = JasperCompileManager.compileReport(relatorioStream);
-        JRSaver.saveObject(jasperReport, nome + ".jasper");
-        return jasperReport;
+    @GetMapping(value = "")
+    public String relatorios(Model model) {
+        return "relatorio/pesquisar";
     }
 
     public Connection abrirConexao() {
         try {
-            System.out.println("4******************************************");
             Class.forName("com.mysql.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/corrida", "root", "");
+            return DriverManager.getConnection("jdbc:mysql://localhost/corrida", "root", "");
 
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -47,30 +42,30 @@ public class RelatorioController {
         return null;
     }
 
+
+    public JasperReport gerarJasper(String relatorio, String nome) throws JRException {
+        InputStream relatorioStream = getClass().getResourceAsStream(relatorio);
+
+        if (relatorioStream == null)
+            return null;
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(relatorioStream);
+        JRSaver.saveObject(jasperReport, nome + ".jasper");
+        return jasperReport;
+    }
+
+
     public JasperPrint exibirRelatorio(JasperReport relatorio, HashMap parametros, Connection conexao) throws JRException {
-        // parametros.put("PAR_codCurso", Integer.parseInt(request.getParameter("txtCodCurso")));
-        System.out.println("3******************************************");
         return JasperFillManager.fillReport(relatorio, parametros, conexao);
     }
 
-    @GetMapping(value = "")
-    public String percursos(Model model){
-        System.out.println("2******************************************");
-        return "relatorio/pesquisar";
-    }
-
-    @GetMapping(value = "{report}")
-    public void gerarRelatorio(HttpServletRequest request, HttpServletResponse response, @PathVariable String report) throws JRException, IOException, SQLException {
-        String nomeRelatorio = report;
-        System.out.println("1******************************************");
-        System.out.printf(report);
+    @GetMapping(value = "{nomeRelatorio}")
+    public void gerarRelatorio(HttpServletRequest request, HttpServletResponse response, @PathVariable String nomeRelatorio) throws JRException, IOException, SQLException {
         Connection conexao = this.abrirConexao();
-
 
         JasperReport relatorio = this.gerarJasper("/reports/" + nomeRelatorio + ".jrxml", nomeRelatorio);
         if (relatorio != null) {
             HashMap parametros = new HashMap();
-            this.exibirRelatorio(relatorio, parametros, conexao);
             byte[] relat = JasperExportManager.exportReportToPdf(this.exibirRelatorio(relatorio, parametros, conexao));
             response.setHeader("Content-Disposition", "attachment;filename=" + nomeRelatorio + ".pdf");
             response.setContentType("application/pdf");
@@ -79,5 +74,28 @@ public class RelatorioController {
         conexao.close();
 
     }
+
+
+    @GetMapping(value = "gerar/{report}/{tipo}/{parametro}")// site.com/administrador/edit/1/
+    public void gerarRelatorioCorridaEstado(HttpServletRequest request, HttpServletResponse response,
+                                            @PathVariable String report,@PathVariable String tipo,
+                                            @PathVariable String parametro) throws JRException, IOException, SQLException {
+        String nomeRelatorio = report;
+        Connection conexao = this.abrirConexao();
+
+        JasperReport relatorio = this.gerarJasper("/reports/" + nomeRelatorio + ".jrxml", nomeRelatorio);
+        if (relatorio != null) {
+            HashMap parametros = new HashMap();
+
+            parametros.put("P_"+tipo, parametro);
+
+            JasperPrint JP = JasperFillManager.fillReport(relatorio, parametros, conexao);
+            byte[] relat = JasperExportManager.exportReportToPdf(this.exibirRelatorio(relatorio, parametros, conexao));
+            response.setHeader("Content-Disposition", "attachment;filename=" + nomeRelatorio + ".pdf");
+            response.setContentType("application/pdf");
+            response.getOutputStream().write(relat);
+        }
+        conexao.close();
+    }
+
 }
-*/
